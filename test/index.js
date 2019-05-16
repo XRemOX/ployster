6ea92f3a739d3987b4316b0b207bfd713c8d8694
Dmitry <dmitry.zhidenkov@gmail.com>
2019-05-16 16:01:53 +0200

    sending registration data to backend

--- test/index.js
+++ test/index.js
@@ -2,6 +2,16 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// WEB
 app.use(express.static('web/public', { index: "login.html" }))
+
+// API
+app.post("/api/register", function(req, res) {
+  console.log(req.body);
+});
 
 app.listen(port, () => console.log(`Ployster is running on port ${port}!`))
