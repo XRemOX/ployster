7f66397bfb4c4489e2f869294694ba21c1e14e44
Dmitry <dmitry.zhidenkov@gmail.com>
2019-05-16 16:27:07 +0200

    add sqlite3 database

--- test/index.js
+++ test/index.js
@@ -6,6 +6,14 @@ var bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+var sqlite3 = require('sqlite3').verbose();
+let db = new sqlite3.Database('./server/ployster.db', (err) => {
+  if (err) {
+    console.error(err.message);
+  }
+  console.log('Connected to the Ployster database.');
+});
+
 // WEB
 app.use(express.static('web/public', { index: "login.html" }))
 
@@ -14,4 +22,6 @@ app.post("/api/register", function(req, res) {
   console.log(req.body);
 });
 
-app.listen(port, () => console.log(`Ployster is running on port ${port}!`))
+app.listen(port, () => console.log(`Ployster is running on port ${port}!`));
+
+db.close();
